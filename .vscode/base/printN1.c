b93f0d5afe8f64b1703db1921d4b2f26a052502f
ZingP <605156398@qq.com>
2018-06-11 11:16:24 +0800

    base

---
+++ .vscode/base/printN1.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+void  PrintN1(int N) {
+    int i;
+    for (i=1; i<=N; i++) {
+        printf("%d\n", i);
+    }
+    return;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    PrintN1(N);
+    return 0;
+}
