db1b80ca0903694a4467a061091b881795e2ab0e
ZingP <605156398@qq.com>
2019-01-02 11:33:31 +0800

    data structure

---
+++ dataStructure/firstBase/base1/printN2.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+void PrintN2(int N){
+    if (N) {
+        PrintN2(N-1);
+        printf("%d\n", N);
+    }
+    return;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    PrintN2(N);
+    return 0;
+}
